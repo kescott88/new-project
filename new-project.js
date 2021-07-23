50dbaf840ecd429863987fba6b2dc441684c1b97
Katie Escott <kwalt48@gmail.com>
2021-07-23 19:03:13 +0100

    add random number generator

--- new-project.js
+++ new-project.js
@@ -1,2 +1,13 @@
 console.log('Am I working?')
 console.log(4 + 5)
+const ranNum = () => {
+    let random = Math.floor(Math.random() * 3)
+    return random
+    if (random = '1') {
+        console.log('Hurray!')
+    } else if (random = '2') {
+        console.log('Wahoo!')
+    } else {
+        console.log('Yesssss!')
+    }
+  }
