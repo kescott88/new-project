9bf9bc7a6b3b78e39a307c270419ad5de9c3459a
Katie Escott <kwalt48@gmail.com>
2021-07-23 19:18:36 +0100

    change function

--- new-project.js
+++ new-project.js
@@ -1,13 +1,16 @@
 console.log('Am I working?')
 console.log(4 + 5)
 const ranNum = () => {
-    let random = Math.floor(Math.random() * 3)
-    return random
-    if (random = '1') {
-        console.log('Hurray!')
-    } else if (random = '2') {
-        console.log('Wahoo!')
-    } else {
-        console.log('Yesssss!')
-    }
-  }
+    let random = Math.floor(Math.random() * 4)
+    if (random === 0) {
+      console.log('hiii')
+    } else if (random === 1) {
+      console.log('yoooo')
+    } else if (random === 2) {
+      console.log('yipeeeee') 
+       } else {
+          console.log('ya ya ya')
+      }
+    };
+
+  ranNum()
